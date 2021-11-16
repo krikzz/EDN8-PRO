af0c7ffe5d803da7e11aca1b3cc74fd9724355c9
krikzz <biokrik@gmail.com>
2021-11-16 19:02:39 +0200

    Update backup.bat

--- backup.bat
+++ backup.bat
@@ -3,7 +3,7 @@ set name=EDN8-PRO
 set filename=%name%-[%date%].7z
 set home=\\datahub\krikzz\work\projects
 mkdir %home%\%name%
-7z -mhe a %filename% * -probomanus68K!robomanus68K!
+7z -mhe a %filename% * -probomanus68K!robomanus68K! -x!.git
 copy %filename% %home%\%name%\%filename%
 del %filename%
 
