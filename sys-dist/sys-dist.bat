e76a92b8714335b049b8767cab7c274a2e7bca02
krikzz <biokrik@gmail.com>
2022-05-19 19:08:32 +0200

    new dist structure

--- sys-dist/sys-dist.bat
+++ sys-dist/sys-dist.bat
@@ -1,4 +1,4 @@
-E:\projects\EDN8-PRO\_tools\flashprog\flashprog\bin\Release\flashprog -ufile E:\projects\EDN8-PRO\N8-OS\nesos.nes menu-cpu.bin
+E:\projects\EDN8-PRO\_tools\flashprog\flashprog\bin\Release\flashprog -ufile E:\projects\EDN8-PRO\modules\boot2\boot2.nes menu-cpu.bin
 E:\projects\EDN8-PRO\_tools\flashprog\flashprog\bin\Release\flashprog -ufile E:\projects\EDN8-PRO\mappers\255\output_files\top.rbf menu-fpg.bin
 E:\projects\EDN8-PRO\_tools\flashprog\flashprog\bin\Release\flashprog -ufile E:\projects\EDN8-PRO\modules\arm-io\build\edio.bin iocore.bin
 pause
